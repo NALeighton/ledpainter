a68c539bdf305c85c7d7a22dc3788781ee71d82c
Nicholas Leighton <naleighton@gmail.com>
2018-10-16 09:41:19 -0400

    Cleaned up firmware

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -26,7 +26,7 @@
 #include <dmx.h>
 #include <power_mgt.h>
 
-CRGB leds[696];
+CRGB leds[696]; //The amount of LEDs on the pannel
 int colors[] = {CRGB::White, CRGB::Red, CRGB::Green, CRGB::Purple, CRGB::Blue, CRGB::Yellow};
 int currColor = 0;
 const int limit = 1000;
@@ -39,6 +39,7 @@ void setup() {
   pinMode(3, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(2), change, FALLING);
   attachInterrupt(digitalPinToInterrupt(3), reset, FALLING);
+  //The LEDs were split into 3 banks
   FastLED.addLeds<NEOPIXEL, 7>(leds, 0, 336);
   FastLED.addLeds<NEOPIXEL, 8>(leds, 336, 336*2);
   FastLED.addLeds<NEOPIXEL, 9>(leds, 336*2, 696);
@@ -58,11 +59,8 @@ void loop() {
   if(Serial.available())
   {
     Serial.readBytes(tmp,3);
-    //Serial.println(i);
-      int val = (tmp[0]-48)*100 + (tmp[1]-48) * 10 + tmp[2]-48;
-//      if(leds[val] == (CRGB)CRGB::White) leds[val] = CRGB::Black;
-      //else 
-      leds[val] = colors[currColor];
+    int val = (tmp[0]-48)*100 + (tmp[1]-48) * 10 + tmp[2]-48; //Read finger postition
+    leds[val] = colors[currColor]; //Use change() function to have the LEDs turn off after a certain time
     FastLED.show();
   }
 }
@@ -87,6 +85,4 @@ void reset()
   {
     leds[i] = CRGB::Black;
   }
-  //FastLED.show();
 }
-
